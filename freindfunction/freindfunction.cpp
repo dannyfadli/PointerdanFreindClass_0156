0eeb64932153617345e6248e6b96ed8b269855e4
dannyfadli <danyfdly95@gmail.com>
2024-05-07 11:15:01 +0700

    menambahkan class mahasiswa dengan private nama sedang publik set nama bukan objek

---
+++ freindfunction/freindfunction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    string nama;
+
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+int main()
+{
+    
+}
